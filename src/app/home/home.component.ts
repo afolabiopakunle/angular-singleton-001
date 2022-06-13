997a488690f43477624a4b8c00cdca06d92aa09f
afolabiopakunle <opakunleafolabi@gmail.com>
2022-06-13 22:50:46 +0100

    getters and setters work

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ColorService } from '../color.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private colorService: ColorService) { }
 
   ngOnInit() {
+  }
+
+  get getColor() {
+    return this.colorService.color;
+  }
+  
+  set setColor(colorValue) {
+    this.colorService.color = colorValue;
   }
 
 }
