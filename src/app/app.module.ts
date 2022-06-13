8076e4ca4412d21609b1dbf0aaf535917549b4b7
afolabiopakunle <opakunleafolabi@gmail.com>
2022-06-13 22:26:36 +0100

    components declared in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HomeComponent, ProfileComponent, ContactComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
